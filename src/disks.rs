8540743dbd76fc297d9540d91c3a417ae921cef8
soyart <artdrawin@gmail.com>
2023-08-26 01:23:58 +0700

    nothing: rename var

--- src/disks.rs
+++ src/disks.rs
@@ -11,13 +11,13 @@ pub fn do_disks(disks: &[manifest::ManifestDisk]) -> Result<(), AliError> {
 }
 
 fn do_disk(disk: &manifest::ManifestDisk) -> Result<(), AliError> {
-    let create_table_cmd = linux::fdisk::create_table_cmd(&disk.device, &disk.table);
-    linux::fdisk::run_fdisk_cmd(&disk.device, &create_table_cmd)?;
+    let cmd_create_table = linux::fdisk::create_table_cmd(&disk.device, &disk.table);
+    linux::fdisk::run_fdisk_cmd(&disk.device, &cmd_create_table)?;
 
     for (n, part) in disk.partitions.iter().enumerate() {
-        let create_part_cmd = linux::fdisk::create_partition_cmd(&disk.table, n + 1, part);
+        let cmd_create_part = linux::fdisk::create_partition_cmd(&disk.table, n + 1, part);
 
-        linux::fdisk::run_fdisk_cmd(&disk.device, &create_part_cmd)?;
+        linux::fdisk::run_fdisk_cmd(&disk.device, &cmd_create_part)?;
     }
 
     Ok(())
