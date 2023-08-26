1aff8e67e5b42d312138eacfe913c762a1e24d6d
soyart <artdrawin@gmail.com>
2023-08-27 03:22:49 +0700

    feat: apply LVM and mkfs commands

--- src/linux/lvm.rs
+++ src/linux/lvm.rs
@@ -1,16 +1,31 @@
-use humanize_rs::bytes::Bytes;
-
 use crate::errors::AliError;
 use crate::manifest;
+use crate::utils::shell::exec;
 
+/// Executes:
+/// pvcreate ${{ pv }}
 pub fn create_pv(pv: &str) -> Result<(), AliError> {
-    Err(AliError::NotImplemented)
+    exec("pvcreate", &[pv])
 }
 
-pub fn create_vg(vg: &str, pvs: &[&str]) -> Result<(), AliError> {
-    Err(AliError::NotImplemented)
+/// Executes:
+/// vgcreate ${{ vg.name }} ${{ vg.pvs }}
+pub fn create_vg(vg: &manifest::ManifestLvmVg) -> Result<(), AliError> {
+    let mut arg = vec![vg.name.as_str()];
+    let pvs = vg.pvs.iter().map(|pv| pv.as_str());
+    arg.extend(pvs);
+
+    exec("vgcreate", &arg)
 }
 
-pub fn create_lv(lv: &str, pv: &str, size: Option<Bytes>) -> Result<(), AliError> {
-    Err(AliError::NotImplemented)
+/// Executes:
+// lvcreate -L ${{ lv.size }} ${{ lv.vg }} -n ${{ lv.name }}
+// lvcreate -l 100%FREE ${{ lv.vg }} -n ${{ lv.name }}
+pub fn create_lv(lv: &manifest::ManifestLvmLv) -> Result<(), AliError> {
+    let (size_flag, size) = match &lv.size {
+        Some(size) => ("-L", size.as_str()),
+        None => ("-l", "100FREE"),
+    };
+
+    exec("lvcreate", &[size_flag, size, "-n", &lv.name])
 }
--- src/linux/mkfs.rs
+++ src/linux/mkfs.rs
@@ -1,5 +1,12 @@
 use crate::errors::AliError;
+use crate::manifest;
+use crate::utils::shell::exec;
 
-pub fn mkfs(fstype: &str) -> Result<(), AliError> {
-    Err(AliError::NotImplemented)
+pub fn create_fs(fs: manifest::ManifestFs) -> Result<(), AliError> {
+    let mkfs_cmd = format!("mkfs.{}", fs.fs_type);
+    match fs.fs_opts {
+        None => exec(&mkfs_cmd, &[fs.device.as_str()]),
+        // @TODO: find ways to spread opts
+        Some(opts) => exec(&mkfs_cmd, &[&opts, fs.device.as_str()]),
+    }
 }
