bdbf9eda0ebdad74814e9016f7feea6f604b98ef
soyart <artdrawin@gmail.com>
2023-09-23 02:02:09 +0700

    refactor: stage application

--- src/ali/apply/mod.rs
+++ src/ali/apply/mod.rs
@@ -10,73 +10,60 @@ mod routines;
 use std::collections::HashSet;
 
 use crate::ali::Manifest;
-use crate::entity::stage::{Stage, StageActions};
+use crate::entity::stage::{self, Stage, StageActions};
 use crate::errors::AliError;
 
 /// Use `manifest` to install a new system to `install_location`
 /// skipping any stages in `skip`, and maps `AliError::ApplyError`
 /// to `AliError::InstallError` with StageActions embedded.
+#[rustfmt::skip]
 pub fn apply_manifest(
     manifest: &Manifest,
     install_location: &str,
     skip: HashSet<Stage>,
 ) -> Result<Box<StageActions>, AliError> {
+
     let mut progress = Box::default();
 
-    if !skip.contains(&Stage::Mountpoints) {
-        if let Err(err) = apply_stages::mountpoints(manifest, install_location, &mut progress) {
+    for stage in stage::STAGES {
+        if skip.contains(&stage) {
+            continue;
+        }
+
+        let result = match stage {
+            Stage::Mountpoints => {
+                apply_stages::mountpoints(manifest, install_location, &mut progress)
+            }
+
+            Stage::Bootstrap => {
+                apply_stages::bootstrap(manifest, install_location, &mut progress)
+            },
+
+            Stage::Routines => {
+                apply_stages::routines(manifest, install_location, &mut progress)
+            },
+
+            Stage::ChrootAli => {
+                apply_stages::chroot_ali(manifest, install_location, &mut progress)
+            },
+
+            Stage::ChrootUser => {
+                apply_stages::chroot_user(manifest, install_location, &mut progress)
+            }
+
+            Stage::PostInstallUser => {
+                apply_stages::postinstall_user(manifest, install_location, &mut progress)
+            }
+        };
+
+        if let Err(err) = result {
             return Err(AliError::InstallError {
                 error: Box::new(err),
                 stages_performed: progress,
-            });
+            })
         }
     }
 
-    if !skip.contains(&Stage::Bootstrap) {
-        if let Err(err) = apply_stages::bootstrap(manifest, install_location, &mut progress) {
-            return Err(AliError::InstallError {
-                error: Box::new(err),
-                stages_performed: progress,
-            });
-        }
-    }
-
-    if !skip.contains(&Stage::Routines) {
-        if let Err(err) = apply_stages::routines(manifest, install_location, &mut progress) {
-            return Err(AliError::InstallError {
-                error: Box::new(err),
-                stages_performed: progress,
-            });
-        }
-    }
-
-    if !skip.contains(&Stage::ChrootAli) {
-        if let Err(err) = apply_stages::chroot_ali(manifest, install_location, &mut progress) {
-            return Err(AliError::InstallError {
-                error: Box::new(err),
-                stages_performed: progress,
-            });
-        }
-    }
-
-    if !skip.contains(&Stage::ChrootUser) {
-        if let Err(err) = apply_stages::chroot_user(manifest, install_location, &mut progress) {
-            return Err(AliError::InstallError {
-                error: Box::new(err),
-                stages_performed: progress,
-            });
-        }
-    }
-
-    if !skip.contains(&Stage::PostInstallUser) {
-        if let Err(err) = apply_stages::postinstall_user(manifest, install_location, &mut progress)
-        {
-            return Err(AliError::InstallError {
-                error: Box::new(err),
-                stages_performed: progress,
-            });
-        }
-    }
 
     Ok(progress)
 }
