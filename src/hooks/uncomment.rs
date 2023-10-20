b80fe775eb30b6012fb4a7f3e5cb7c8303a5773e
artnoi43 <artdrawin@gmail.com>
2023-10-21 02:47:09 +0700

    refactor: use dynamic hook key

--- src/hooks/uncomment.rs
+++ src/hooks/uncomment.rs
@@ -73,7 +73,7 @@ impl Hook for HookUncomment {
     }
 
     fn parse_cmd(&mut self, s: &str) -> Result<(), AliError> {
-        let uc = parse_uncomment(s)?;
+        let uc = parse_uncomment(&self.hook_key(), s)?;
         self.uc = Some(uc);
 
         Ok(())
@@ -191,8 +191,12 @@ fn apply_uncomment(
     })?;
 
     let uncommented = match mode {
-        Mode::All => uncomment_text_all(&original, &uc.marker, &uc.pattern),
-        Mode::Once => uncomment_text_once(&original, &uc.marker, &uc.pattern),
+        Mode::All => {
+            uncomment_text_all(hook_key, &original, &uc.marker, &uc.pattern)
+        }
+        Mode::Once => {
+            uncomment_text_once(hook_key, &original, &uc.marker, &uc.pattern)
+        }
     }?;
 
     match mode_hook {
@@ -213,6 +217,7 @@ fn apply_uncomment(
 }
 
 fn uncomment_text_all(
+    _hook_key: &str,
     original: &str,
     marker: &str,
     key: &str,
@@ -235,6 +240,7 @@ fn uncomment_text_all(
 }
 
 fn uncomment_text_once(
+    hook_key: &str,
     original: &str,
     marker: &str,
     key: &str,
@@ -253,7 +259,7 @@ fn uncomment_text_once(
     }
 
     Err(AliError::HookError(format!(
-        "{KEY_UNCOMMENT}: no such comment pattern '{marker} {key}'"
+        "{hook_key}: no such comment pattern '{marker} {key}'"
     )))
 }
 
@@ -264,18 +270,21 @@ fn uncomment_text_once(
 /// Examples:
 /// @uncomment PubkeyAuthentication /etc/ssh/sshd_config
 /// => Uncomments key PubkeyAuthentication in /etc/ssh/sshd_config
-fn parse_uncomment(hook_cmd: &str) -> Result<Uncomment, AliError> {
+fn parse_uncomment(
+    hook_key: &str,
+    hook_cmd: &str,
+) -> Result<Uncomment, AliError> {
     let parts = shlex::split(hook_cmd);
     if parts.is_none() {
         return Err(AliError::BadHookCmd(format!(
-            "{KEY_UNCOMMENT}: bad cmd {hook_cmd}"
+            "{hook_key}: bad cmd {hook_cmd}"
         )));
     }
 
     let parts = parts.unwrap();
     if parts.len() < 3 {
         return Err(AliError::BadHookCmd(format!(
-            "{KEY_UNCOMMENT}: expect at least 2 arguments"
+            "{hook_key}: expect at least 2 arguments"
         )));
     }
 
@@ -290,7 +299,7 @@ fn parse_uncomment(hook_cmd: &str) -> Result<Uncomment, AliError> {
         5 => {
             if parts[2] != "marker" {
                 return Err(AliError::BadHookCmd(format!(
-                    "{KEY_UNCOMMENT}: unexpected argument {}, expecting 2nd argument to be `marker`",
+                    "{hook_key}: unexpected argument {}, expecting 2nd argument to be `marker`",
                     parts[2],
                 )));
             }
@@ -303,7 +312,7 @@ fn parse_uncomment(hook_cmd: &str) -> Result<Uncomment, AliError> {
         }
         l => {
             Err(AliError::BadHookCmd(format!(
-                "{KEY_UNCOMMENT}: bad cmd parts: {l}"
+                "{hook_key}: bad cmd parts: {l}"
             )))
         }
     }
@@ -332,17 +341,23 @@ fn test_uncomment_text_all() {
     let expected = r#"Port 22
 PubkeyAuthentication no"#;
 
+    let hook_key = "@uncomment-all";
     for original in originals {
-        let uncommented_port = uncomment_text_all(original, "#", "Port")
-            .expect("failed to uncomment Port");
+        let uncommented_port =
+            uncomment_text_all(hook_key, original, "#", "Port")
+                .expect("failed to uncomment Port");
 
         if original == uncommented_port {
             panic!("'# Port' not uncommented");
         }
 
-        let uncommented_all =
-            uncomment_text_all(&uncommented_port, "#", "PubkeyAuthentication")
-                .expect("failed to uncomment PubkeyAuthentication");
+        let uncommented_all = uncomment_text_all(
+            hook_key,
+            &uncommented_port,
+            "#",
+            "PubkeyAuthentication",
+        )
+        .expect("failed to uncomment PubkeyAuthentication");
 
         if original == uncommented_all {
             panic!("'# PubkeyAuthentication not uncommented'");
@@ -364,13 +379,19 @@ fn test_uncomment_text_once() {
     let expected = r#"Port 22
 PubkeyAuthentication no"#;
 
+    let hook_key = "@uncomment";
     for original in originals {
-        let uncommented_port = uncomment_text_once(original, "#", "Port")
-            .expect("failed to uncomment Port");
+        let uncommented_port =
+            uncomment_text_once(hook_key, original, "#", "Port")
+                .expect("failed to uncomment Port");
 
-        let uncommented_all =
-            uncomment_text_once(&uncommented_port, "#", "PubkeyAuthentication")
-                .expect("failed to uncomment PubkeyAuthentication");
+        let uncommented_all = uncomment_text_once(
+            hook_key,
+            &uncommented_port,
+            "#",
+            "PubkeyAuthentication",
+        )
+        .expect("failed to uncomment PubkeyAuthentication");
 
         assert_ne!(expected, uncommented_port);
         assert_ne!(original, uncommented_all);
